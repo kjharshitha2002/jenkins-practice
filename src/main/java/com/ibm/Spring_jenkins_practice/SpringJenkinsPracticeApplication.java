c689a962db32c5481a8e6725b16a0c3a2439daa5
Harshitha K J <harshitha.k.j2@ibm.com>
2026-05-15 11:27:00 +0530

    project committed

---
+++ src/main/java/com/ibm/Spring_jenkins_practice/SpringJenkinsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.Spring_jenkins_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/Spring_jenkins_practice/SpringJenkinsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.Spring_jenkins_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
